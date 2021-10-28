ae48d69565ffd72a05ae8ab280d00dab24eb10d8
xonfps <isaque.souza@lunes.io>
2021-10-28 19:51:01 -0300

    fixed: remove the edit this page button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,18 +92,6 @@ module.exports = {
     [
       '@docusaurus/preset-classic',
       {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
