b29d6c25083658b6652381c7ab955f37445d37c3
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-01-05 04:14:40 +0000

    removed: 🔭 from title tab

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -26,10 +26,10 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Lunes ${siteConfig.title}`}
+      title={`Lunes ${siteConfig.title.slice(3)}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
